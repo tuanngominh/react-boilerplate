96c9e45b022c6334be6434853ca935d22e9fa08a
tuanngominh <tuan@trexanhlab.com>
2017-03-08 09:45:28 +0700

    add material-ui

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,27 @@
-import { configure } from '@kadira/storybook';
+import React from 'react'
+import { configure, addDecorator } from '@kadira/storybook'
+
+// Needed for onTouchTap of material-ui.com components
+import injectTapEventPlugin from 'react-tap-event-plugin'
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+//Add decorator to init Material UI theme
+const WrapMuiThemeProvider = (story) => {
+  //hack so hot reload still working
+  try {
+    injectTapEventPlugin()
+  } catch (e) {
+
+  }
+    
+  return (
+    <MuiThemeProvider>
+      {story()}
+    </MuiThemeProvider>
+  )
+}
+
+addDecorator(WrapMuiThemeProvider)
 
 function loadStories() {
   require('../src/stories');
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,17 +1,8 @@
 import React from 'react';
 import { storiesOf, action, linkTo } from '@kadira/storybook';
-import Button from './Button';
-import Welcome from './Welcome';
+import RaisedButton from 'material-ui/RaisedButton'
 
-storiesOf('Welcome', module)
-  .add('to Storybook', () => (
-    <Welcome showApp={linkTo('Button')}/>
-  ));
-
-storiesOf('Button', module)
-  .add('with text', () => (
-    <Button onClick={action('clicked')}>Hello Button</Button>
+storiesOf('RaisedButton', module)
+  .add('Primary', () => (
+    <RaisedButton label="Primary" primary={true} />
   ))
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
-  ));
